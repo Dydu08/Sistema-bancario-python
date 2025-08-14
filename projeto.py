f2b2aff84d953bf88b95b4bd4f7684f2cad0c678
Dydu08 <eduardo08dias@gmail.com>
2025-08-14 23:20:28 +0000

    Projeto

---
+++ projeto.py
@@ -0,0 +1,20 @@
+#Sistema bancario
+
+
+
+while true:
+    print('Digite 1 para cadastrar')
+    print('Digite 3 para depositar ')
+    print('Digite 3 para sacar')
+    print('Digite 4 para extrato')
+    print('Digite 5 para sair')
+    in = int(input('-> '))
+             
+    match in:
+    
+    
+    
+    
+    
+    
+             
